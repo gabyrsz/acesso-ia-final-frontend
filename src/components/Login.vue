5eb7e0fb82776f033b8cfd0f44a15b37c01f3b7b
gabyrsz <gabriellerossi.ifmg@gmail.com>
2025-03-10 20:07:03 -0300

    configurando vue-router

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+Login
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+        };
+    },
+    methods:{
+    }
+};
+</script>
+
+<style scoped></style>
